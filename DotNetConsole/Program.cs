0cd64811135da8936b28076f12e8abe79965ff25
Eva <36014601+eeevvaaa@users.noreply.github.com>
2020-01-22 15:33:46 -0600

    Add files via upload

---
+++ DotNetConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am going to the store today and I am getting " + args[0]);
+            Console.WriteLine("Is there anything else you would like to get from the store?");
+            string storeItem = Console.ReadLine();
+            Console.WriteLine("We will also get " + storeItem + " from the store");
+        }
+    }
+}
